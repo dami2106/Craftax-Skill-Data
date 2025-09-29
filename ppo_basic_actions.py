a16eb5651df8cb9f80d574c1462f90e7c61dc5f9
dami2106 <2303289@students.wits.ac.za>
2025-09-29 13:59:48 +0200

    new PPO logic

--- ppo_basic_actions.py
+++ ppo_basic_actions.py
@@ -4,6 +4,7 @@ from gymnasium.wrappers import TimeLimit
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecTransposeImage, VecMonitor
+from stable_baselines3.common.callbacks import EvalCallback
 from top_down_env_gymnasium import CraftaxTopDownEnv
 
 import imageio
@@ -67,29 +68,42 @@ if __name__ == "__main__":
     train_env = VecTransposeImage(train_env)  # HWC -> CHW for CnnPolicy
     train_env = VecMonitor(train_env)
 
-    model = PPO.load("ppo_craftax_wood_ppo_actions")
 
-    # model = PPO(
-    #     policy="CnnPolicy",
-    #     env=train_env,
-    #     seed=SEED,  # set SB3/torch RNGs too for full determinism
-    #     verbose=1,
-    #     tensorboard_log="./tb_logs_ppo_craftax",
-    #     device="auto",
-    # )
-
-    # model.learn(
-    #     total_timesteps=500_000,
-    #     log_interval=10,
-    #     tb_log_name="ppo_wood_actions",
-    #     progress_bar=True,
-    # )
-    # model.save("ppo_craftax_wood_ppo_actions")
-
-    # -------- Eval vec env (choose a seed; use same wrapper for fixed-seed eval) --------
     eval_env = DummyVecEnv([make_env(seed=SEED)])
     eval_env = VecTransposeImage(eval_env)
     eval_env = VecMonitor(eval_env)
+    
+    # Evaluation callback: evaluate every eval_freq steps on eval_env and save the best model
+    eval_callback = EvalCallback(
+        eval_env,
+        eval_freq=500,
+        n_eval_episodes=5,
+        deterministic=True,
+        render=False,
+    )
+    
+    # model = PPO.load("ppo_craftax_wood_ppo_actions")
+
+    model = PPO(
+        policy="CnnPolicy",
+        env=train_env,
+        seed=SEED,  # set SB3/torch RNGs too for full determinism
+        verbose=1,
+        tensorboard_log="./tb_logs_ppo_craftax",
+        device="auto",
+    )
+
+    model.learn(
+        total_timesteps=500_000,
+        log_interval=10,
+        tb_log_name="ppo_wood_actions",
+        progress_bar=True,
+        callback=eval_callback,
+    )
+    model.save("ppo_craftax_wood_ppo_actions")
+
+    # -------- Eval vec env (choose a seed; use same wrapper for fixed-seed eval) --------
+    
 
     obs = eval_env.reset()
     images = [to_gif_frame(obs)]
