262c80482958745f45f5af104d4837d89580b68f
Dami <2303289@students.wits.ac.za>
2025-10-02 19:23:17 +0200

    Update run_all.sh

--- Scripts/run_all.sh
+++ Scripts/run_all.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=gen_craftax          # Job name
 #SBATCH --partition=bigbatch                     # Replace with your cluster's GPU partition name
-#SBATCH --output=/home-mscluster/dharvey/HiSD/generate_data.out  # Standard output and error log
+#SBATCH --output=/home-mscluster/dharvey/HiSD/generate_data_sp_static.out  # Standard output and error log
 # Load your environment
 
 source ~/.bashrc
-conda activate craftax
+conda activate SOTA
 
 echo "Starting data generation..."
 
-python get_truth_stats.py
-echo "Truth stats generated."
+# python get_truth_stats.py
+# echo "Truth stats generated."
 
 
-# python train_pca_model.py --components 512
+
+python train_pca_model.py --components 650
 # python train_pca_model.py --components 750
 # python train_pca_model.py --components 1000
 # python train_pca_model.py --components 2000
 # echo "PCA model trained."
 
 
-# python get_pca_features.py --components 512
+python get_pca_features.py --components 650
 # python get_pca_features.py --components 750
 # python get_pca_features.py --components 1000
 # python get_pca_features.py --components 2000
