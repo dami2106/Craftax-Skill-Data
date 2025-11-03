f711eafb08ab1cf6bda979989f75867f1ca5dd15
dami2106 <2303289@students.wits.ac.za>
2025-11-03 14:04:43 +0200

    Scripts

--- RL_Scripts/ppo_basic_actions.sh
+++ RL_Scripts/ppo_basic_actions.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=ppo_craftax_actions        # Job name
 #SBATCH --partition=bigbatch                  # Replace with your cluster's GPU partition name
-#SBATCH --output=/home-mscluster/dharvey/HiSD/rl_experiments/ppo_basic_actions_woodpick_all_seeds.out  # Combined output log
+#SBATCH --output=/home-mscluster/dharvey/HiSD/rl_experiments/ppo_basic_actions_woodpick_all_seeds_2.out  # Combined output log
 
 # Load your environment
 source ~/.bashrc
 conda activate SOTA
 
 # Define your list of seeds
-seeds=(888 0 333 9 42)
+# seeds=(888 0 333 9 42)
+seeds=(2106 1 404 1408 506)
 
 # Loop through each seed
 for seed in "${seeds[@]}"; do
--- RL_Scripts/ppo_options_compile.sh
+++ RL_Scripts/ppo_options_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=ppo_craftax_options_compile     # Job name
 #SBATCH --partition=bigbatch                     # Replace with your cluster's GPU partition name
-#SBATCH --output=/home-mscluster/dharvey/HiSD/rl_experiments/ppo_options_compile_woodpick_all_seeds.out  # Standard output and error log
+#SBATCH --output=/home-mscluster/dharvey/HiSD/rl_experiments/ppo_options_compile_woodpick_all_seeds_2.out  # Standard output and error log
 # Load your environment
 
 source ~/.bashrc
@@ -9,7 +9,8 @@ conda activate SOTA
 
 
 # Define your list of seeds
-seeds=(888 0 333 9 42)
+# seeds=(888 0 333 9 42)
+seeds=(2106 1 404 1408 506)
 
 # Loop through each seed
 for seed in "${seeds[@]}"; do
